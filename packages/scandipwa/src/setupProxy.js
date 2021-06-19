84a1fbe9c3765847f9307dde50d895e9a3be2d4c
root <root@marketihor.optim.biz>
2021-06-19 14:30:21 +0200

    Ihor

---
+++ packages/scandipwa/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://my.market.ro',
+      changeOrigin: true,
+    })
+  );
+};
